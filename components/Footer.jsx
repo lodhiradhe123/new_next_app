ef2f4c24170ad8c1c7f3bdea13202f3851a0308a
radhe <radheshyamlodhi48@gmail.com>
2024-07-24 16:01:20 +0530

    nextjs

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='w-full h-18 bg-zinc-300 text-3xl font-semibold flex justify-center items-center absolute bottom-0 p-20'>Footer</div>
+  )
+}
+
+export default Footer
---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='w-full h-12 text-5xl font-semibold p-10 bg-zinc-300 flex justify-center items-center'> Header</div>
+  )
+}
+
+export default Header
